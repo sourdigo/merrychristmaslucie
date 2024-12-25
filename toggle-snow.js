b16fb57e8c0813463ddbe128a3d720f336a20b82
sourdigo <davidcui824@gmail.com>
2024-12-25 05:36:11 -0600

    Delete toggle-snow.js

--- toggle-snow.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("toggle-snow").addEventListener("click", function () {
-    const snowElement = document.getElementById("snow");
-    if (snowElement.style.display === "none") {
-        snowElement.style.display = "block";
-    } else {
-        snowElement.style.display = "none";
-    }
-});
